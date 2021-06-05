f883fc17fba13598a1bbf7674101e6c72b240ea7
ernesto <ernesto002@icloud.com>
2021-06-04 22:08:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const testVar = {}
 
+function testFunc() {
+    return "hi"
+}
+
+const superbowlWin = (array) =>{
+    const result= array.find(x=>{
+        if(x.result==="W"){return x.year}
+    })
+    if(result===undefined){return undefined}
+    else{return result.year}
+}
